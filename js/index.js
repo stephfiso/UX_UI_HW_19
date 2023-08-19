d5edb5e77bcb18fbd08069b933a00479cf0b585f
stephfiso <121734523+stephfiso@users.noreply.github.com>
2023-08-19 17:11:02 -0600

    mod 19  changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 console.log("YOU WIN, KID :)");
 $(document).ready(function (){
     $("nav ul li").click(function() {
-        $('html, body').animate({
-            scrollTop: $(".myWork").offset().top
-        }, 2000);
+        if ($(this).text().toLowerCase() === "work") {
+            $('html, body').animate({
+                scrollTop: $(".myWork").offset().top
+            }, 2000);
+        }
+    });
+});
+$(document).ready(function () {
+    $("#hamburger").click(function () {
+        $("#menu").toggleClass("show");
     });
 });
